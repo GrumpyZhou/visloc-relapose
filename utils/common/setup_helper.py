efed2567eae5ea79e7e6db555a72b9966d39de9b
Qunjie Zhou <jennyzqj00@gmail.com>
2019-08-02 15:50:02 +0200

    Add util functions for printing and exporting data

--- utils/common/setup_helper.py
+++ utils/common/setup_helper.py
@@ -42,3 +42,45 @@ def config2str(config):
         confstr += '{}:{}{}'.format(k, str(v), separator)
     confstr += '----------------------------------------{}'.format(separator)
     return confstr
+
+def vec2str(vec, end=''):
+    """Convert a 1D vector to string
+    Values are seperated by an empty space.
+    An ending token is appended at the end of the string. 
+    Default ending token is empty.
+    Example:
+       vec: (1, 2, 3) 
+       output: '1 2 3'
+    """
+    
+    assert isinstance(vec, (np.ndarray, list, tuple))
+    if isinstance(vec, np.ndarray):
+        vec = np.squeeze(vec)
+    str_vec = ''
+    for i, v in enumerate(vec):
+        str_vec += '{:.8f}'.format(v)
+        if i != len(vec)-1:
+            str_vec += ' '
+    str_vec = str_vec + end
+    return str_vec
+
+
+def array2str(arr, end=''):
+    """Convert a 2D array to string
+    Values are seperated by an empty space.
+    Each row is seperated by '\n'
+    An ending token is appended at the end of the string. 
+    Default ending token is empty.
+    Example:
+       arr: [[1, 2, 3],
+             [4, 5, 6]]
+       output: '1 2 3\n4 5 6\n'
+    """
+    
+    assert isinstance(arr, np.ndarray)
+    rows = arr.shape[0]
+    str_arr = ''
+    for i in range(rows):
+        str_arr += vec2str(arr[i], end='\n')
+    str_arr = str_arr + end
+    return str_arr
