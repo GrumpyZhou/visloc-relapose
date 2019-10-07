f59b0cef472d07de02a7f1b48baa71c388a5d856
Qunjie Zhou <jennyzqj00@gmail.com>
2019-10-07 04:24:44 +0200

    Update measure.py

--- utils/eval/measure.py
+++ utils/eval/measure.py
@@ -1,5 +1,4 @@
 import numpy as np
-import torch
 import math
 from transforms3d.quaternions import quat2mat, mat2quat
 
@@ -25,13 +24,18 @@ normF = lambda F: F / F[-1,-1] # Normalize F by the last value
 normalize = lambda A:  A / np.linalg.norm(A)
 
 def expand_homo_ones(arr2d, axis=1):
+    """Raise 2D array to homogenous coordinates
+    Args:
+        - arr2d: (N, 2) or (2, N)
+        - axis: the axis to append the ones
+    """    
     if axis == 0:
         ones = np.ones((1, arr2d.shape[1]))
     else:
         ones = np.ones((arr2d.shape[0], 1))      
     return np.concatenate([arr2d, ones], axis=axis)
 
-def symmetric_epipolar_distance(pts1, pts2, F):
+def symmetric_epipolar_distance(pts1, pts2, F, sqrt=False):
     """Calculate symmetric epipolar distance between 2 sets of points
     Args:
         - pts1, pts2: points correspondences in the two images, 
@@ -46,12 +50,17 @@ def symmetric_epipolar_distance(pts1, pts2, F):
     # Homogenous coordinates
     pts1 = expand_homo_ones(pts1, axis=1)
     pts2 = expand_homo_ones(pts2, axis=1)
-
+    
     # l2=F*x1, l1=F^T*x2
     l2 = np.dot(F, pts1.T) # 3,N
     l1 = np.dot(F.T, pts2.T)
     dd = np.sum(l2.T * pts2, 1)  # Distance from pts2 to l2
-    d = np.abs(dd) * (1.0 / np.sqrt(l1[0, :] ** 2 + l1[1, :] ** 2) + 1.0 / np.sqrt(l2[0, :] ** 2 + l2[1, :] ** 2))
+    
+    if sqrt:
+        # Adopted from  DFM paper.
+        d = np.abs(dd) * (1.0 / np.sqrt(l1[0, :] ** 2 + l1[1, :] ** 2) + 1.0 / np.sqrt(l2[0, :] ** 2 + l2[1, :] ** 2))
+    else: # Original version    
+        d = dd ** 2 * (1.0 / (l1[0, :] ** 2 + l1[1, :] ** 2) + 1.0 /(l2[0, :] ** 2 + l2[1, :] ** 2))
     return d
 
 '''Error metric computation'''
