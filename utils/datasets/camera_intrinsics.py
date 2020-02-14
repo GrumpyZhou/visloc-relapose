a076fdab09e15e3af35f6b643a276f8997f54918
Qunjie Zhou <jennyzqj00@gmail.com>
2020-02-14 16:20:05 +0100

    Revert "Update ncmatch, intrinsic scaling; Updat intrinsics and dataloading"
    This reverts commit 7690941bea8e6c9a214885ca31e8056a4d85b752.

--- utils/datasets/camera_intrinsics.py
+++ utils/datasets/camera_intrinsics.py
@@ -1,24 +1,20 @@
 import os
 import numpy as np
 
-def get_camera_intrinsic_loader(base_dir, dataset, scene, wt, ht):
+def get_camera_intrinsic_loader(base_dir, dataset, scene):
     if 'Cambridge' in dataset:
-        return CambridgeIntrinsics(base_dir, scene, wt=wt, ht=ht)
+        return CambridgeLandmarks(base_dir, scene)
     elif '7Scenes' in dataset:
-        return Scenes7Intrinsics(base_dir, scene, wt=wt, ht=ht)
+        return Scenes7(base_dir, scene)
     
-class CambridgeIntrinsics:
-    scenes = ['KingsCollege', 'OldHospital', 'ShopFacade', 'StMarysChurch']
-    def __init__(self, base_dir, scene, wt=1920, ht=1080, w=1920, h=1080):
+class CambridgeLandmarks:
+    scenes = ['ShopFacade', 'KingsCollege', 'OldHospital', 'StMarysChurch']
+    def __init__(self, base_dir, scene):
         assert scene in self.scenes
         self.base_dir = base_dir
         self.scene = scene
-        self.wt, self.ht = wt, ht
-        self.w, self.h = w, h
-        self.ox, self.oy = w / 2, h / 2
-        self.sK = np.array([[wt / w, 0, 0],
-                            [0, ht / h, 0],
-                            [0, 0, 1]])
+        self.w, self.h = 1920, 1080
+        self.ox, self.oy = 960, 540
         self.focals = self.get_focals()
 
     def get_focals(self):
@@ -44,82 +40,55 @@ class CambridgeIntrinsics:
             im_list = list(self.focals.keys())
         for im in im_list:
             f = self.focals[im]
-            K = np.array([[f, 0, self.ox], 
-                          [0, f, self.oy], 
-                          [0, 0, 1]], dtype=np.float32)
-            K = self.sK.dot(K)
-            matrices[im] = K 
-        return matrices   
+            K = np.array([[f, 0, self.ox], [0, f, self.oy], [0, 0, 1]], dtype=np.float32) 
+            matrices[im] = K
+        return matrices
     
     def get_relative_intrinsic_matrix(self, im1, im2):
         f1 = self.focals[im1]
         f2 = self.focals[im2]        
         f = (f1 + f2) / 2.0    # Compute mean focal of two images
-        K = np.array([[f, 0, self.ox], 
-                      [0, f, self.oy], 
-                      [0, 0, 1]])
-        K = self.sK.dot(K)        
+        fx, fy = f, f
+        K = np.array([[f, 0, self.ox], [0, f, self.oy], [0, 0, 1]]) 
         return K
 
-class Scenes7Intrinsics:
-    scenes = ['chess', 'fire', 'heads', 'office', 'pumpkin', 'redkitchen', 'stairs']
-    def __init__(self, base_dir, scene, wt=640, ht=480, w=640, h=480):
+class Scenes7:
+    scenes = ['heads', 'chess', 'fire', 'office', 'pumpkin', 'redkitchen', 'stairs']
+    def __init__(self, base_dir, scene):
         assert scene in self.scenes
         self.base_dir = base_dir
         self.scene = scene
-        self.wt, self.ht = wt, ht
-        self.w, self.h = w, h
-        self.ox, self.oy = w / 2, h / 2
-        self.sK = np.array([[wt / w, 0, 0],
-                            [0, ht / h, 0],
-                            [0, 0, 1]])
+        self.w, self.h = 640, 480
+        self.ox, self.oy = 320, 240
         self.f = 585
-        self.K = self.sK.dot(np.array([[self.f, 0, self.ox], [0, self.f, self.oy], [0, 0, 1]], dtype=np.float32))
+        self.K = np.array([[self.f, 0, self.ox], [0, self.f, self.oy], [0, 0, 1]], dtype=np.float32) 
 
-    def get_intrinsic_matrices(self, im_list=None):
-        if im_list is None:
-            return self.K
-        else:
-            return {im:self.K for im in im_list}
+    def get_intrinsic_matrices(self, im_list):
+        matrices = {im :self.K for im in im_list}
+        return matrices
     
     def get_relative_intrinsic_matrix(self, *args):
         return self.K
 
-class PragueIntrinsics:
+class Prague:
     scenes = ['castle_paradise_garden', 'castle_yard', 'charles_square_station_escalator',
               'dancing_house', 'st_georges_basilica_building', 'st_vitus_cathedral',
               'street_facades', 'zurich_eth_hg_rows']
-    @staticmethod
-    def get_imsize(base_dir, scene):        
-        intrinsics = os.path.join(base_dir, scene,'intrinsics.txt')
-        ff = open(intrinsics, 'r')
-        params = ff.readlines()[3].split()
-        w, h = [float(p) for p in params[2:4]]
-        ff.close()
-        return w, h            
-
-    def __init__(self, base_dir, scene, wt=1557, ht=642):
+    
+    def __init__(self, base_dir, scene):
         assert scene in self.scenes
         self.base_dir = base_dir
         self.scene = scene
         intrinsics = os.path.join(self.base_dir, self.scene,'intrinsics.txt')
         ff = open(intrinsics, 'r')
         params = ff.readlines()[3].split()
-        self.w, self.h, self.fx, self.fy = [float(p) for p in params[2:6]]
+        self.w, self.h, self.fx, self.fy, self.ox, self.oy = [float(p) for p in params[2::]]
         ff.close()
-        
-        self.wt, self.ht = wt, ht        
-        self.ox, self.oy = self.w / 2, self.h / 2
-        self.sK = np.array([[self.wt / self.w, 0, 0],
-                            [0, self.ht / self.h, 0],
-                            [0, 0, 1]])        
-        self.K = self.sK.dot(np.array([[self.fx, 0, self.ox], [0, self.fy, self.oy], [0, 0, 1]], dtype=np.float32))        
+        self.K = np.array([[self.fx, 0, self.ox], [0, self.fy, self.oy], [0, 0, 1]], dtype=np.float32) 
 
-    def get_intrinsic_matrices(self, im_list=None):
-        if im_list is None:
-            return self.K
-        else:
-            return {im:self.K for im in im_list}
+    def get_intrinsic_matrices(self, im_list):
+        matrices = {im : self.K for im in im_list}
+        return matrices
     
     def get_relative_intrinsic_matrix(self, *args):
         return self.K
